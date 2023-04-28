8902797381b3cb65ea6eacec7d62ce30cb2cb59a
MargoSZC <128178578+MargoSZC@users.noreply.github.com>
2023-04-28 15:27:56 -0300

    aula metodo buscar e validação e chave estrangeira

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Usuario;
+use App\Models\Categoria;
+
+class UsuarioController extends Controller
+{
+   function index(){
+
+        $usuarios = Usuario::All();
+       // dd($usuarios);
+
+		return view("UsuarioList")->with(["usuarios"=> $usuarios]);
+	 }
+
+    function create(){
+        $categorias = Categoria::orderBy('nome')->get();
+
+        return view("UsuarioForm")->with(["categorias"=> $categorias]);
+     }
+
+     function store(Request $request){
+        $request->validate([
+            'nome'=>"required | max: 120",
+            'telefone'=>"required | max: 20",
+            'email'=>"nullable | email | max: 100",
+    ],
+['nome.required'=>"O nome é obrigatório",
+'nome.max'=>"O nome é limitado a 120 caracteres",
+'telefone.required'=>"O telefone é obrigatório",
+'telefone.max'=>"O telefone é limitado a 20 caracteres",
+'email.max'=>"O email é limitado a 100 caracteres",
+]
+);
+      //dd( $request->nome);
+        Usuario::create([
+            'nome'=> $request->nome,
+            'telefone'=> $request->telefone,
+            'email'=> $request->email]);
+
+        return \redirect()->action("App\Http\Controllers\UsuarioController@index");
+     }
+
+     function edit($id){
+        $usuario = Usuario::findOrFail($id);
+        //dd($usuario);
+        $categorias = Categoria::orderBy('nome')->get();
+        return view("UsuarioForm")->with(['usuario'=> $usuario,
+        "categorias"=> $categorias
+    ]);
+     }
+
+     function update(Request $request){
+        //dd( $request->nome);
+          Usuario::updateOrCreate(['id'=>$request->id], [
+              'nome'=> $request->nome,
+              'telefone'=> $request->telefone,
+              'email'=> $request->email]);
+
+          return \redirect()->action("App\Http\Controllers\UsuarioController@index");
+        }
+//
+
+     function destroy($id){
+        $usuario = Usuario::findOrFail($id);
+
+        $usuario->delete();
+
+        return \redirect()->action("App\Http\Controllers\UsuarioController@index");
+     }
+
+     function search(Request $request){
+if($request->campo == "nome"){
+        $usuarios = Usuario::where('nome', 'like', '%'.$request->valor.'%')->get();
+    }
+    else {
+        $usuarios = Usuario::all();
+    }
+       // dd($usuarios);
+
+		return view("UsuarioList")->with(["usuarios"=> $usuarios]);
+	 }
+}
